3ffb5fd20cf029bdd36f26c093d628f85d25d28a
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-06-18 16:02:52 -0700

    WIP

--- src/game.tsx
+++ src/game.tsx
@@ -15,6 +15,11 @@ interface Sprite {
     y: number,
 }
 
+const defaultSprite: Sprite = {
+    img: "",
+    x: 0, y: 0,
+}
+
 export interface GameSyncState {
     events: GameEvent[],
     tick: number,
@@ -33,7 +38,7 @@ interface GameProps {
 
 export function Game({queueInteraction, syncState, onUpdateSyncState}: GameProps) {
     const {events, tick} = syncState;
-    const [sprites, setSprites] = useState<{[k: string]: Sprite}>({});
+    const [sprites, setSprites] = useState<{ [k: string]: Sprite }>({});
     const [startTime, _] = useState(() => Date.now() - tick);
 
     useEffect(() => {
@@ -43,7 +48,20 @@ export function Game({queueInteraction, syncState, onUpdateSyncState}: GameProps
             idx = idx === -1 ? idx = events.length : idx;
 
             if (idx > 0) {
-                
+                events.slice(0, idx).forEach(([event]) => {
+                    if (event.type === "update") {
+                        setSprites(sprites => ({
+                            ...sprites,
+                            [event.payload.id]: {...defaultSprite, ...event.payload.sprite}
+                        }));
+                    } else if (event.type === "remove") {
+                        setSprites(sprites => {
+                            const newSprites = {...sprites};
+                            delete newSprites[event.payload.id];
+                            return newSprites;
+                        });
+                    }
+                });
             }
         }, 100);
 
